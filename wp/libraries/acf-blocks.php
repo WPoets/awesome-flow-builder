a8a866f6400e61af2bdffc3bd4d1cf4a25d8e382
Amit Singh <amit@wpoets.com>
2026-08-25 12:52:13 +0530

    updated support for ACF Blocks V3

--- wp/libraries/acf-blocks.php
+++ wp/libraries/acf-blocks.php
@@ -1,56 +1,62 @@
 <?php
 namespace aw2\acf_blocks;
 
-add_action('init','aw2\acf_blocks\setup_acf_blocks',1);
-add_action('acf/init', 'aw2\acf_blocks\register_acf_blocks',15);
+add_action('init', 'aw2\acf_blocks\setup_acf_blocks', 1);
+add_action('acf/init', 'aw2\acf_blocks\register_acf_blocks', 15);
 
+// Apply this filter in your theme's functions.php or a custom functionality plugin
+add_filter('acf/blocks/default_block_version', function ($default_version, $block) {
+    return 3;
+}, 10, 2);
 
-
-if ( version_compare( $GLOBALS['wp_version'], '5.8-alpha-1', '<' ) ) {
-    add_filter( 'block_categories', 'aw2\acf_blocks\add_block_category', 10, 2 );
+if (version_compare($GLOBALS['wp_version'], '5.8-alpha-1', '<')) {
+    add_filter('block_categories', 'aw2\acf_blocks\add_block_category', 10, 2);
 } else {
-    add_filter( 'block_categories_all', 'aw2\acf_blocks\add_block_category', 10, 2 );
+    add_filter('block_categories_all', 'aw2\acf_blocks\add_block_category', 10, 2);
 }
 
 
-function setup_acf_blocks(){
-	\awesome_flow::run_core('gutenberg-blocks');
+function setup_acf_blocks()
+{
+    \awesome_flow::run_core('gutenberg-blocks');
 }
 
 
-function register_acf_blocks(){
-	$gutenberg_blocks=&\aw2_library::get_array_ref('gutenberg_blocks');
-	if( function_exists('acf_register_block_type') ){
-		  
-		  foreach($gutenberg_blocks as $key=>$block){
-            if(isset($block['acf_block']) && $block['acf_block']===true){
+function register_acf_blocks()
+{
+    $gutenberg_blocks =& \aw2_library::get_array_ref('gutenberg_blocks');
+    if (function_exists('acf_register_block_type')) {
+
+        foreach ($gutenberg_blocks as $key => $block) {
+            if (isset($block['acf_block']) && $block['acf_block'] === true) {
                 acf_register_block_type($block);
-			    unset($gutenberg_blocks[$key]);
+                unset($gutenberg_blocks[$key]);
             }
-		    
-		  }
-		  
-		 
-	}
+
+        }
+
+
+    }
 }
 
 
- 
+
 /**
  * Adding a new (custom) block category.
  *
  * @param   array                   $block_categories       Array of categories for block types.
  * @param   WP_Block_Editor_Context $block_editor_context   The current block editor context.
  */
-function add_block_category( $block_categories, $block_editor_context ) {
+function add_block_category($block_categories, $block_editor_context)
+{
     return array_merge(
         array(
             array(
-                'slug'  => 'awesome-gt-blocks',
-                'title' => __( 'Awesome UI Blocks', 'text-domain' ),
-                'icon' =>'schedule'
+                'slug' => 'awesome-gt-blocks',
+                'title' => __('Awesome UI Blocks', 'text-domain'),
+                'icon' => 'schedule'
             ),
         ),
-		$block_categories
+        $block_categories
     );
 }
